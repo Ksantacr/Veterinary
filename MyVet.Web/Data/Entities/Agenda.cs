c82bdb85eb51ef755657cacd332a1c3722340713
Kevin Santacruz <defonix0@gmail.com>
2019-09-07 10:59:17 -0500

    Agenda fix DataFormatString

--- MyVet.Web/Data/Entities/Agenda.cs
+++ MyVet.Web/Data/Entities/Agenda.cs
@@ -9,10 +9,10 @@ namespace MyVet.Web.Data.Entities
     public class Agenda
     {
         public int Id { get; set; }
-        [Display(Name = "Born")]
-        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [Display(Name = "Date")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
         [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd hh:mm tt}", ApplyFormatInEditMode = true)]
         public DateTime Date { get; set; }
         public string Remarks { get; set; }
         public DateTime DateLocal => Date.ToLocalTime();
