a366001541e3ef93f8067dff801e411a548b3948
Kevin Santacruz <defonix0@gmail.com>
2019-09-07 10:59:36 -0500

    run seed on startup

--- MyVet.Web/Startup.cs
+++ MyVet.Web/Startup.cs
@@ -36,7 +36,7 @@ namespace MyVet.Web
             services.AddDbContext<DataContext>(cfg=> {
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
-
+            services.AddTransient<SeedDb>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
